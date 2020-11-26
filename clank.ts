9b204432373f69dc24023ab81384d074f887f9a2
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2020-11-26 00:00:57 +0000

    Formatting

--- clank.ts
+++ clank.ts
@@ -1,44 +1,69 @@
+// Imports
 let Discord = require("discord.js");
 let config = require("./config.json");
 let client = new Discord.Client();
 let timeTaken = Date.now();
 
-const currentGames: string[] = ['Apex', 'Crusader Kings', 'Warzone', 'Tabletop Simulator', 'Talisman', 'Luke decides!', 'Eddie decides!', 'Michael decides!'];  
-
-console.log("Powering on...");
-console.log(chooseRandomGame());
-
+// Variables
+const currentGames: string[] = [
+  "Apex",
+  "Crusader Kings",
+  "Warzone",
+  "Tabletop Simulator",
+  "Tekken",
+  "Talisman",
+  "Luke decides!",
+  "Eddie decides!",
+  "Michael decides!",
+];
 const prefix = "!";
 
-client.on("message", function(message) { 
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
-  
-    if (command === "check") {
-      const timeTaken = Date.now() - message.createdTimestamp;
-      message.reply(`Hello Eddie! It took ${timeTaken}ms before I received your command.`);
-    }
-  
-    else if (command === "game") {
-      console.log(chooseRandomGame);
-      message.reply(chooseRandomGame());
-    }
+console.log("Powering on...");
 
-    message.reply(`Farewell, I shall return!`);
+// Main Command Definitions
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+    case "timeTaken": {
+      const time = Date.now() - message.createdTimestamp;
+      message.reply(checkTime(time));
+      break;
+    }
+    case "game": {
+      message.reply(chooseRandomGame);
+      break;
+    }
+    case "shutdown": {
+      message.reply("Farewell, I shall return!");
+      break;
+    }
+    default: {
+      message.reply("I'm sorry but I have no idea what you are talking about!");
+      break;
+    }
+  }
 });
 
-
+// Functions
 function chooseRandomGame(): string {
-    let numCurrentGame: number = currentGames.length;
-    let randomNumber: number =  Math.random() * (numCurrentGame - 0) + 0;
-    let game: string = currentGames[Math.round(randomNumber)];
-    console.log(randomNumber)
-    return game;
+  let numCurrentGame: number = currentGames.length;
+  let randomNumber: number = Math.random() * (numCurrentGame - 0) + 0;
+  let game: string = currentGames[Math.round(randomNumber)];
+  console.log(randomNumber);
+  return game;
 }
 
+function checkTime(time): string {
+  return (
+    "Hello Eddie! It took " + timeTaken + "ms before I received your command."
+  );
+}
+
+// Login
 client.login(config.BOT_TOKEN);
