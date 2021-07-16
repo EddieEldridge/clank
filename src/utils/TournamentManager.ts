74bc472a5e44a4618640538aba6d75e648299128
Eddie Eldridge <eddie.eldridge@genesys.com>
2021-07-16 12:24:51 +0100

    Change case

--- src/utils/TournamentManager.ts
+++ src/utils/TournamentManager.ts
@@ -1,14 +1,14 @@
 import Tournament from './models/tekken/Tournament'
 import { TekkenStages, TekkenCharacters } from './constants/tekken/TekkenData'
-import { pickRandomElementFromArray } from './utils/Utils';
+import { pickRandomElementFromArray } from './utils/utils';
 import Match from '../models/tekken/Match';
 
 export function tournamentStartingMessage(): string {
     try {
-        const helpCommands: string = 
+        const helpCommands: string =
         '=== Tekken Tournament Manager ===\n' +
-        '*What kind of tournament do you want to play?*\n' + 
-        '1. *Lightning* - Define a given word\n' + 
+        '*What kind of tournament do you want to play?*\n' +
+        '1. *Lightning* - Define a given word\n' +
         '2. *Standard* - Roll a dice. Same format as Roll20\n' +
         '3. *Epic* - Roll a dice. Same format as Roll20\n';
         return helpCommands;
@@ -18,7 +18,7 @@ export function tournamentStartingMessage(): string {
 }
 
 export function createNewTournament(matchType: string) {
-    
+
     // 1. Decide how many rounds each player will play
     const numberOfRounds: number = determineNumberOfRounds(matchType);
 
@@ -26,31 +26,31 @@ export function createNewTournament(matchType: string) {
     const tournament = new Tournament(numberOfRounds);
 
     // 3. Decide who will play and how many points the match is worth and who they will have to play
-    const match = 
+    const match =
     tournament.matches.add()
 
     // 4. Wait until the match is finished
 
-    // 5. Store the results in the Tournament 
+    // 5. Store the results in the Tournament
 
-    // 6. Repeat steps 3-5 until every player has played an equal number of rounds 
+    // 6. Repeat steps 3-5 until every player has played an equal number of rounds
 
-    
+
 
 }
 
 
 export function addMatchToTournament(matchType: string) {
-    
+
 
 
     // 4. Wait until the match is finished
 
-    // 5. Store the results in the Tournament 
+    // 5. Store the results in the Tournament
 
-    // 6. Repeat steps 3-5 until every player has played an equal number of rounds 
+    // 6. Repeat steps 3-5 until every player has played an equal number of rounds
 
-    
+
 
 }
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -61,4 +61,4 @@ export function printResponse(response: any) {
 export function capitalize (s: string): string {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
-  }
+}
