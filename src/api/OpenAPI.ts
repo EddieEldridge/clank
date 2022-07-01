b5af65f97a6ff2963a919a402756060c3527c514
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2022-07-01 12:00:31 +0100

    Update OpenAPI max_tokens

--- src/api/OpenAPI.ts
+++ src/api/OpenAPI.ts
@@ -11,16 +11,13 @@ const openai = new OpenAIApi(openAIConfig);
 export default class OpenAPI {
     async getCompletion(thingToComplete: string): Promise<any> {
 
-        console.log(process.env.OPENAI_API_KEY);
-
         const completion = await openai.createCompletion({
             model: "text-davinci-002",
             prompt: thingToComplete,
-            max_tokens: 300
+            max_tokens: 600
         });
 
         const predicition = completion?.data?.choices[0].text
-
 
         return predicition;
     }
